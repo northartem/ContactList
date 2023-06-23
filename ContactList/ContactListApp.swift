54ee61fb819e7b7e8746132425770d81b5d948ec
AssanCred <temix@live.ru>
2023-06-23 20:47:05 +0700

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Артём Латушкин on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
